b65e9c726f789d0665e92868f3fcd3e7ce1830ca
Rúben Santos <ruben.santos@codacy.com>
2022-11-14 17:45:18 +0000

    First commit with cov

---
+++ src/main/java/com/codacy/utils/Math.java
@@ -0,0 +1,33 @@
+package com.codacy.utils;
+
+public class Math {
+
+    private int magicNumber;
+
+    public Math(int magicNumber) {
+        this.magicNumber = magicNumber;
+    }
+
+    /**
+     * Adds 2 numbers, unless there is a magic number on the second argument
+     */
+    public int magicAdd(int x, int y) {
+        if (y == magicNumber) {
+            System.out.println("hey");
+            System.out.println("hey");
+            System.out.println("hey");
+            System.out.println("hey");
+            System.out.println("hey");
+            System.out.println("hey");
+            return y - x;
+        } else {
+            System.out.println("hey");
+            System.out.println("hey");
+            System.out.println("hey");
+            System.out.println("hey");
+            System.out.println("hey");
+            System.out.println("hey");
+            return x + y;
+        }
+    }
+}
